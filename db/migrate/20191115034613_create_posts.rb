20ac3d1d961d3ceda816ade8552e0faac38ecbbf
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-11-15 13:45:09 +0900

    cretae_table

--- db/migrate/20191115034613_create_posts.rb
+++ db/migrate/20191115034613_create_posts.rb
@@ -3,8 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title
       t.string :address
-      t.text :content, limit: 16777215
-      t.references :user, foreign_key: true      
+      t.text :content, limit: 16777215    
       t.timestamps
     end
   end
