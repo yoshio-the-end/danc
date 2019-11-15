dd4ce034d2bd152b1b685a276489d13027f199dc
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-11-15 13:24:13 +0900

    initial comit

---
+++ db/migrate/20191115034613_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :address
+      t.text :content, limit: 16777215
+      t.references :user, foreign_key: true      
+      t.timestamps
+    end
+  end
+end
