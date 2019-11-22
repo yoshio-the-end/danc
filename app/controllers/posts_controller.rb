5d3df2ff8ea252cea3475b19b884dcb8970cb0e6
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-11-22 19:34:27 +0900

    create_new_action
    新規投稿をできるようにした。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to action: :index
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text, :category_id).merge(user_id: current_user.id)
+  end
+
 end
