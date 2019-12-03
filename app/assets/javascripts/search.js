af5b6275a599d0b607f94ba99d92ea909ebb2921
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-12-03 12:34:35 +0900

    Update search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,23 +2,23 @@ $(function() {
 
   var search_list = $(".index__main");
   function appendPost(post) {
-    var html = `     <div class="index__main__contents">
-                        <img src="${post.image}">
-                      <div class="index__main__contents__text">
-                        <a href="${post.address}" class="index__main__contents__text__title">${post.title}</a>
-                        <div class="index__main__contents__text__description">
-                        <p>${post.content}</p>
-                        </div>
-                      </div>
-                      <div class="index__main__contents__user">
-                        <div class="index__main__contents__user__name">
-                        ${post.name}
-                        </div>
-                        <div class="index__main__contents__user__date">
-                        ${post.date}
-                        </div>
-                      </div>
-                    </div>`
+    var html = `<div class="index__main__contents">
+                  <img src="${post.image}">
+                <div class="index__main__contents__text">
+                  <a href="${post.address}" class="index__main__contents__text__title">${post.title}</a>
+                  <div class="index__main__contents__text__description">
+                  <p>${post.content}</p>
+                  </div>
+                </div>
+                <div class="index__main__contents__user">
+                  <div class="index__main__contents__user__name">
+                  ${post.name}
+                  </div>
+                  <div class="index__main__contents__user__date">
+                  ${post.date}
+                  </div>
+                </div>
+              </div>`
   search_list.append(html);
   }
 
